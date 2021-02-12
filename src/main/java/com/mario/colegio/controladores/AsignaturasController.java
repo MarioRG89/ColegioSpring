56239fbc047b469b59529157a3fdc187e019ef80
mario <mrebollogarcia89@gmail.com>
2021-02-12 13:16:12 +0100

    [ADD] insertar asisgnaturas

--- src/main/java/com/mario/colegio/controladores/AsignaturasController.java
+++ src/main/java/com/mario/colegio/controladores/AsignaturasController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mario.colegio.daos.CombosDAO;
+import com.mario.colegio.entidades.AsignaturasEntity;
+import com.mario.colegio.repositories.AsignaturaRepository;
 
 @Controller
 public class AsignaturasController {
@@ -13,9 +17,23 @@ public class AsignaturasController {
 	@Autowired
 	private CombosDAO combo;
 	
+	@Autowired
+	private AsignaturaRepository asignaturaRepository;
+	
 	@GetMapping(value = "insertarasignaturas")
-	public String formularioInsertarAsignaturas(ModelMap model) {
-		model.addAttribute("comboAlumnos", combo.comboMunicipios());
-		return "vistas/alumnos/insertarAsignaturas";
+	public String formularioInsertarAsignaturas() {
+		
+		return "vistas/asignaturas/insertarAsignaturas";
 	}
+	
+	@PostMapping(value = "insertarasignaturas")
+	public String InsertarAsignaturas(@RequestParam(value = "id", required = false) Integer id,
+			@RequestParam("nombre") String nombre, @RequestParam(value = "curso") Integer curso,
+			@RequestParam(value = "tasa") Double tasa, ModelMap model) {
+		AsignaturasEntity a= new AsignaturasEntity(id, nombre, curso, tasa);
+		asignaturaRepository.save(a);
+		return "vistas/asignaturas/insertarAsignaturas";
+	}
+	
+	
 }
--- src/main/java/com/mario/colegio/repositories/AsignaturaRepository.java
+++ src/main/java/com/mario/colegio/repositories/AsignaturaRepository.java
@@ -1,11 +1,22 @@
 package com.mario.colegio.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.mario.colegio.dtos.Asignaturas;
 import com.mario.colegio.entidades.AsignaturasEntity;
 
 
 public interface AsignaturaRepository extends CrudRepository<AsignaturasEntity, Integer> {
-	
+	@Query(value = "select new com.mario.colegio.dtos.Asignaturas (a.id, a.nombre, a.curso, a.tasa) "
+			+ "FROM com.mario.colegio.entidades.AsignaturasEntity a where "
+			+ "(a.id LIKE CONCAT('%',:id,'%') or :id is null) "
+			+ "AND a.nombre LIKE CONCAT ('%',:nombre,'%') " 
+			+ "AND a.curso LIKE  CONCAT ('%',:curso,'%') "
+			+ "AND a.tasa  LIKE CONCAT ('%',:tasa,'%') ")
+	List<Asignaturas>buscaAsignaturas(@Param("id")Integer id,@Param("nombre")String nombre);
 	
 }
