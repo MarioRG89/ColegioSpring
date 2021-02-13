3836aa8015b0c2534036b2f9f0323c99044200f8
mario <mariorebollogarcia89@gmail.com>
2021-02-13 13:55:43 +0100

    [ADD] modificar notas

--- src/main/java/com/mario/colegio/controladores/NotasController.java
+++ src/main/java/com/mario/colegio/controladores/NotasController.java
@@ -23,7 +23,7 @@ import com.mario.colegio.repositories.NotasRepository;
 
 @Controller
 public class NotasController {
-	
+
 	@Autowired
 	private CombosDAO combo;
 	@Autowired
@@ -32,57 +32,100 @@ public class NotasController {
 	private AsignaturaRepository asignatura;
 	@Autowired
 	private NotasRepository notasRepository;
-	
-	
-	@GetMapping(value="insertarnota")
-	public String insertarNotaGet( ModelMap model) {
+
+	@GetMapping(value = "insertarnota")
+	public String insertarNotaGet(ModelMap model) {
 		model.addAttribute("listaAlumnos", combo.comboAlumnos());
 		model.addAttribute("listaAsignaturas", combo.comboAsignaturas());
 		return "vistas/notas/insertarNotas";
 	}
-	@PostMapping(value="insertarnota")
-	public String insertarNota(@RequestParam("asignaturas") Integer idAsignatura, @RequestParam(value = "alumnos") Integer idAlumno,
-			@RequestParam(value = "nota") Integer nota,@RequestParam(value="fecha")String fecha ,ModelMap model) {
+
+	@PostMapping(value = "insertarnota")
+	public String insertarNota(@RequestParam("asignaturas") Integer idAsignatura,
+			@RequestParam(value = "alumnos") Integer idAlumno, @RequestParam(value = "nota") Integer nota,
+			@RequestParam(value = "fecha") String fecha, ModelMap model) {
 		if (fecha == "") {
 			Date fecha_Act = new Date();
 			fecha = new SimpleDateFormat("yyyy-MM-dd").format(fecha_Act);
 		}
 		model.addAttribute("listaAlumnos", combo.comboAlumnos());
 		model.addAttribute("listaAsignaturas", combo.comboAsignaturas());
-		Optional<AlumnoEntity> optinalAlumno= alumno.findById(idAlumno);
+		Optional<AlumnoEntity> optinalAlumno = alumno.findById(idAlumno);
 		AlumnoEntity a = optinalAlumno.get();
-		Optional<AsignaturasEntity> optinalAsignatura= asignatura.findById(idAsignatura);
+		Optional<AsignaturasEntity> optinalAsignatura = asignatura.findById(idAsignatura);
 		AsignaturasEntity asig = optinalAsignatura.get();
 		NotasEntity n = new NotasEntity(a, asig, nota, fecha);
 		notasRepository.save(n);
 		return "vistas/notas/insertarNotas";
 	}
-	@GetMapping(value="listadonotas")
+
+	@GetMapping(value = "listadonotas")
 	public String listaNotasGet() {
 		return "vistas/notas/listadoNotas";
 	}
-	@PostMapping(value="listadonotas")
-	public String listaNotas(@RequestParam(value="id",required=false) Integer idAlumno, @RequestParam(value = "nombre",required = false) String nombreAlumno,
-			@RequestParam(value = "asignatura",required = false) String nombreAsignatura,@RequestParam(value = "nota",required=false) Integer nota,@RequestParam(value="fecha",required = false)String fecha ,ModelMap model) {
-		List<Notas> listaNotas=notasRepository.buscaNotas(idAlumno, nota, fecha, nombreAlumno, nombreAsignatura);
-		model.addAttribute("lista",listaNotas);
+
+	@PostMapping(value = "listadonotas")
+	public String listaNotas(@RequestParam(value = "id", required = false) Integer idAlumno,
+			@RequestParam(value = "nombre", required = false) String nombreAlumno,
+			@RequestParam(value = "asignatura", required = false) String nombreAsignatura,
+			@RequestParam(value = "nota", required = false) Integer nota,
+			@RequestParam(value = "fecha", required = false) String fecha, ModelMap model) {
+		List<Notas> listaNotas = notasRepository.buscaNotas(idAlumno, nota, fecha, nombreAlumno, nombreAsignatura);
+		model.addAttribute("lista", listaNotas);
 		return "vistas/notas/listadoNotas";
 	}
-	
-	@GetMapping(value="formularioborrarnota")
+
+	@GetMapping(value = "formularioborrarnota")
 	public String borraNotasFormularioGet() {
-		 return "vistas/notas/borrarNotas";
+		return "vistas/notas/borrarNotas";
 	}
-	@PostMapping(value="formularioborrarnota")
-	public String borraNotasForm( @RequestParam(value = "nombre",required = false) String nombreAlumno,
-			@RequestParam(value = "asignatura",required = false) String nombreAsignatura,@RequestParam(value="fecha",required = false)String fecha ,ModelMap model) {
-		 List<Notas> listaNotas=notasRepository.buscaNotasAlumnoAsignatura(fecha, nombreAlumno, nombreAsignatura);
-		 model.addAttribute("lista",listaNotas);
-		 return "vistas/notas/borrarNotas";
+
+	@PostMapping(value = "formularioborrarnota")
+	public String borraNotasForm(@RequestParam(value = "nombre", required = false) String nombreAlumno,
+			@RequestParam(value = "asignatura", required = false) String nombreAsignatura,
+			@RequestParam(value = "fecha", required = false) String fecha, ModelMap model) {
+		List<Notas> listaNotas = notasRepository.buscaNotasAlumnoAsignatura(fecha, nombreAlumno, nombreAsignatura);
+		model.addAttribute("lista", listaNotas);
+		return "vistas/notas/borrarNotas";
 	}
-	@PostMapping(value="borrarnota")
-	public String borraNota( @RequestParam(value = "idNota",required = false) Integer id) {
-		 notasRepository.deleteById(id);
-		 return "vistas/notas/borrarNotas";
+
+	@PostMapping(value = "borrarnota")
+	public String borraNota(@RequestParam(value = "idNota", required = false) Integer id) {
+		notasRepository.deleteById(id);
+		return "vistas/notas/borrarNotas";
 	}
+
+	@GetMapping(value = "formularioactualizarnota")
+	public String actualizarNotasFormularioGet() {
+		return "vistas/notas/actualizarNotas";
+	}
+
+	@PostMapping(value = "formularioactualizarnota")
+	public String actualizarNotasFormulario(@RequestParam(value = "nombre", required = false) String nombreAlumno,
+			@RequestParam(value = "asignatura", required = false) String nombreAsignatura,
+			@RequestParam(value = "fecha", required = false) String fecha, ModelMap model) {
+		List<Notas> listaNotas = notasRepository.buscaNotasAlumnoAsignatura(fecha, nombreAlumno, nombreAsignatura);
+		model.addAttribute("listaAlumnos", combo.comboAlumnos());
+		model.addAttribute("listaAsignaturas", combo.comboAsignaturas());
+		model.addAttribute("lista", listaNotas);
+		return "vistas/notas/actualizarNotas";
+	}
+
+	@PostMapping(value = "actualizarnota")
+	public String actualizarNotas(@RequestParam(value = "idNota", required = false) Integer id,
+			@RequestParam(value = "alumnos", required = false) Integer idAlumno,
+			@RequestParam(value = "asignaturas", required = false) Integer idAsignatura,
+			@RequestParam(value = "nota", required = false) Integer nota,
+			@RequestParam(value = "fecha", required = false) String fecha, ModelMap model) {
+		model.addAttribute("listaAlumnos", combo.comboAlumnos());
+		model.addAttribute("listaAsignaturas", combo.comboAsignaturas());
+		Optional<AlumnoEntity> optinalAlumno = alumno.findById(idAlumno);
+		AlumnoEntity a = optinalAlumno.get();
+		Optional<AsignaturasEntity> optinalAsignatura = asignatura.findById(idAsignatura);
+		AsignaturasEntity asig = optinalAsignatura.get();
+		NotasEntity n = new NotasEntity(a, asig, nota, fecha);
+		notasRepository.save(n);
+		return "vistas/notas/actualizarNotas";
+	}
+
 }
