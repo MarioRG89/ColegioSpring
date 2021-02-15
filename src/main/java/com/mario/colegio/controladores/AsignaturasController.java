0b9b63871dd8778a4f8fecde81bda706ae2fda94
mario <mariorebollogarcia89@gmail.com>
2021-02-15 18:53:14 +0100

    [FIX]olvido invocar metodo de actualizar asignaturas

--- src/main/java/com/mario/colegio/controladores/AsignaturasController.java
+++ src/main/java/com/mario/colegio/controladores/AsignaturasController.java
@@ -11,72 +11,77 @@ import com.mario.colegio.daos.AsignaturasDAO;
 
 @Controller
 public class AsignaturasController {
-	
-	
+
 	@Autowired
 	private AsignaturasDAO asignatura;
-	
+
 	@GetMapping(value = "insertarasignaturas")
 	public String formularioInsertarAsignaturas() {
-		
+
 		return "vistas/asignaturas/insertarAsignaturas";
 	}
-	
+
 	@PostMapping(value = "insertarasignaturas")
 	public String InsertarAsignaturas(@RequestParam(value = "id", required = false) Integer id,
-			@RequestParam("nombre") String nombre, @RequestParam(value = "curso",required = false) Integer curso,
+			@RequestParam("nombre") String nombre, @RequestParam(value = "curso", required = false) Integer curso,
 			@RequestParam(value = "tasa") Double tasa, ModelMap model) {
 		asignatura.insertarAsignaturas(id, nombre, curso, tasa);
 		return "vistas/asignaturas/insertarAsignaturas";
 	}
+
 	@GetMapping(value = "listadoasignaturas")
 	public String formularioListarAsignaturas() {
-		
+
 		return "vistas/asignaturas/listadoAsignaturas";
 	}
-	@PostMapping(value="listadoasignaturas")
+
+	@PostMapping(value = "listadoasignaturas")
 	public String listarAsignaturas(@RequestParam(value = "id", required = false) Integer id,
 			@RequestParam("nombre") String nombre, @RequestParam(value = "curso", required = false) Integer curso,
 			@RequestParam(value = "tasa", required = false) Double tasa, ModelMap model) {
-		model.addAttribute("lista",asignatura.obtenerTodasAsignaturas(id, nombre, curso, tasa));
+		model.addAttribute("lista", asignatura.obtenerTodasAsignaturas(id, nombre, curso, tasa));
 		return "vistas/asignaturas/listadoAsignaturas";
 	}
+
 	@GetMapping(value = "formularioborrarasignatura")
 	public String formularioBorrarAsignaturas() {
 		return "vistas/asignaturas/borrarAsignaturas";
 	}
+
 	@PostMapping(value = "formularioborrarasignatura")
 	public String formularioListarBorrarAsignaturas(@RequestParam(value = "id", required = false) Integer id,
 			@RequestParam("nombre") String nombre, @RequestParam(value = "curso", required = false) Integer curso,
 			@RequestParam(value = "tasa", required = false) Double tasa, ModelMap model) {
 
-		
-		model.addAttribute("lista",asignatura.obtenerTodasAsignaturas(id, nombre, curso, tasa));
+		model.addAttribute("lista", asignatura.obtenerTodasAsignaturas(id, nombre, curso, tasa));
 		return "vistas/asignaturas/borrarAsignaturas";
 	}
-	@PostMapping(value="borrarasignatura")
-	public String borrarAsignatura(@RequestParam(value = "id",required = false)Integer id) {
+
+	@PostMapping(value = "borrarasignatura")
+	public String borrarAsignatura(@RequestParam(value = "id", required = false) Integer id) {
 		asignatura.borrarAsignatura(id);
 		return "vistas/asignaturas/borrarAsignaturas";
 	}
-	
-	@GetMapping(value="formularioactualizarasignatura")
+
+	@GetMapping(value = "formularioactualizarasignatura")
 	public String formularioActualizarasignatura() {
 		return "vistas/asignaturas/actualizarAsignaturas";
 	}
-	@PostMapping(value="formularioactualizarasignatura")
+
+	@PostMapping(value = "formularioactualizarasignatura")
 	public String formularioListarActualizarasignatura(@RequestParam(value = "id", required = false) Integer id,
 			@RequestParam("nombre") String nombre, @RequestParam(value = "curso", required = false) Integer curso,
 			@RequestParam(value = "tasa", required = false) Double tasa, ModelMap model) {
-		model.addAttribute("lista",asignatura.obtenerTodasAsignaturas(id, nombre, curso, tasa));
+		model.addAttribute("lista", asignatura.obtenerTodasAsignaturas(id, nombre, curso, tasa));
 		return "vistas/asignaturas/actualizarAsignaturas";
 	}
-	@PostMapping(value="actualizarasignatura")
+
+	@PostMapping(value = "actualizarasignatura")
 	public String actualizaAsignatura(@RequestParam(value = "id", required = false) Integer id,
 			@RequestParam("nombre") String nombre, @RequestParam(value = "curso", required = false) Integer curso,
 			@RequestParam(value = "tasa", required = false) Double tasa, ModelMap model) {
-		
-		
+		asignatura.actualizarAsignaturas(id, nombre, curso, tasa);
+
 		return "vistas/asignaturas/actualizarAsignaturas";
 	}
 }
